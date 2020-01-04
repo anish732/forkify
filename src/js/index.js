52b4da4fb401dc64fd0f36674d6a6d0276b5f44b
Shital Patel <shital732@yahoo.com>
2020-01-03 21:24:24 -0500

    render recipe

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,36 @@
-import Search from "../models/Search";
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-const search = new Search("pizza");
+/*Global state of the app
+ *- Search object
+ *- Current recipe object
+ *- Shopping List object
+ *- Liked recipes*/
 
-console.log(search);
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+  // 1. Get query from the view
+
+  const query = searchView.getInput();
+
+  if (query) {
+    //2 New search object and add to state.search
+    state.search = new Search(query);
+
+    //3 Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+    //4 Search for recipes
+    await state.search.getResults();
+
+    //5 Render results on UI
+    searchView.renderResults(state.search.result);
+  }
+};
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});

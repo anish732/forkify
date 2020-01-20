dd0bb1763c58f5990c79b81d13072c532c169ff3
Shital Patel <shital732@yahoo.com>
2020-01-20 17:38:01 -0500

    add shopping list in DOM

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import List from "./models/List";
 
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -102,6 +103,20 @@ const controlRecipe = async () => {
 ["hashchange", "load"].forEach(event =>
   window.addEventListener(event, controlRecipe)
 );
+
+//LIST CONTROLLER
+
+const controlList = () => {
+  // Create a new list IF there in none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
 // Handling recipe button clicks
 
 elements.recipe.addEventListener("click", e => {
@@ -115,7 +130,7 @@ elements.recipe.addEventListener("click", e => {
     // Increase button is clicked
     state.recipe.updateServings("inc");
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn, .recipe__btn--add *")) {
+    controlList();
   }
 });
-
-window.l = new List();
--- src/js/models/List.js
+++ src/js/models/List.js
@@ -6,7 +6,7 @@ export default class List {
   }
 
   addItem(count, unit, ingredient) {
-    const items = {
+    const item = {
       id: uniqid(),
       count,
       unit,
@@ -15,11 +15,11 @@ export default class List {
     this.items.push(item);
     return item;
   }
+
   deleteItem(id) {
     const index = this.items.findIndex(el => el.id === id);
-
-    //[2,4,8,6] splice (1,2) --> returns [4,8], orginal array is [2,6]
-    //[2,4,8,6] slice(1,2) --> returns 4, original array is [2,4,8,6]
+    // [2,4,8] splice(1, 2) -> returns [4, 8], original array is [2]
+    // [2,4,8] slice(1, 2) -> returns 4, original array is [2,4,8]
     this.items.splice(index, 1);
   }
 
